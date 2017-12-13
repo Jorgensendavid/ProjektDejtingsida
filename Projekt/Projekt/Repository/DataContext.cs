a8c1133ec451b91bf4e1147af14ec6b5534aa744
areimertz <amanda@reimertz.se>
2017-12-13 09:47:01 +0100

    databas

---
+++ Projekt/Projekt/Repository/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Projekt.Models;
+
+namespace Projekt.Repository
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+    }
+}
